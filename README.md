54104941990fcad7456b18ecb2ebb370271948b0
unknown <machineroot1337@gmail.com>
2023-03-27 09:39:38 -0700

    error fix

---
+++ README.md
@@ -0,0 +1,33 @@
+# zipcrack
+![zip](https://user-images.githubusercontent.com/82051128/192567533-5ed56f17-6b59-43c9-8491-b9432cb8a03c.png)
+
+
+# Installation:
+    1. git clone https://github.com/machine1337/zipcrack.git
+    2. cd zipcrack 
+    3. python3 -m pip install -r requirements.txt
+    
+# Requirements:
+    1. pyfiglet
+    2. termcolor
+    3. requests
+    
+# Usage:
+    python3 zcrack.py
+    
+# Demo Video:
+https://www.youtube.com/watch?v=miEcCCVI4eM
+
+
+# Features:
+     *: You Can Crack ZIP Password Protected Files Using Below Method:
+     1) Dictionary Attack
+        a) Your Own Custom Wordlist is required.
+        
+     2) More Features Will Be Added IA:)
+        
+ 
+# Author:
+    Coded By: Machine404
+
+         
---
+++ zcrack.py
@@ -0,0 +1,131 @@
+import os
+import platform
+import time
+print("[*] Checking Requirements Module....")
+def header():
+    ascii_banner = pyfiglet.figleet_format("{ZIP CRACKER}").upper()
+    print(colored(ascii_banner.rstrip("\n"), 'cyan', attrs=['bold']))
+    print(colored("                                <Coded By: Clay>     \n", 'yellow', attrs=['bold']))
+    print(colored("                                <Version: 2.0>     \n", 'magenta', attrs=['bold']))
+    return
+if platform.system().startswith("Windows"):
+    try:
+        from tqdm import tqdm
+    except ImportError:
+        os.system("python -m pip install tqdm -q -q -q")
+        from tqdm import tqdm
+    try:
+        import termcolor
+    except ImportError:
+        os.system("python -m pip install termcolor -q -q -q")
+        import termcolor
+    from termcolor import colored
+    try:
+        import pyfiglet
+    except ImportError:
+        os.system("python -m pip install pyfiglet -q -q -q")
+        import pyfiglet
+elif platform.system().startswith("Linux"):
+    try:
+        from tqdm import tqdm
+    except ImportError:
+        os.system("python3 -m pip install tqdm -q -q -q")
+        from tqdm import tqdm
+    try:
+        import termcolor
+    except ImportError:
+        os.system("python3 -m pip install termcolor -q -q -q")
+        import termcolor
+    from termcolor import colored
+    try:
+        import pyfiglet
+    except ImportError:
+        os.system("python3 -m pip install pyfiglet -q -q -q")
+        import pyfiglet
+def linuxpdf():
+    os.system("clear")
+    header()
+    zip_filename = input(termcolor.colored("[*] Enter Path Of Your zip file:- ", 'cyan'))
+    if not os.path.exists(zip_filename):
+        print(termcolor.colored("\n[ X ] File " + zip_filename + " was not found, Provide Valid FileName And Path!",
+                                'red'))
+        exit()
+    print(termcolor.colored("\n[*] Analyzing Zip File:- ", 'blue'), zip_filename)
+    time.sleep(1)
+    if zip_filename[-3:] == "zip":
+        print(termcolor.colored("\n[ ✔ ] Valid ZIP File Found...", 'green'))
+    else:
+        print(termcolor.colored("\n[ X ] This is not a valid .zip file...\n", 'red'))
+        exit()
+    pwd_filename = input(termcolor.colored("\nEnter Path Of Your Wordlist:- ", 'yellow'))
+    if not os.path.exists(pwd_filename):
+        print(termcolor.colored("\n[ X ] File " + pwd_filename + " was not found, Provide Valid FileName And Path!",
+                                'red'))
+        exit()
+    with open(pwd_filename, "rb") as passwords:
+        passwords_list = passwords.readlines()
+        total_passwords = len(passwords_list)
+        my_zip_file = zipfile.ZipFile(zip_filename)
+        for index, password in enumerate(passwords_list):
+            try:
+                my_zip_file.extractall(path="Extracted Folder", pwd=password.strip())
+                print(colored("\n{***********************SUCCESS***********************}", 'green'))
+                print(colored("[ ✔ ] ZIP FILE Password Found:- ", 'cyan'), password.decode().strip())
+                break
+            except:
+                helo = round((index / total_passwords) * 100, 2)
+                if helo == '100%':
+                    print(colored("[ X ] ALL ATTEMPTS FAILED", 'red'))
+                else:
+                    print(colored(f"[*] Trying password:- {password.decode().strip()} ", 'green'))
+                continue
+def winpdf():
+    os.system("cls")
+    header()
+    zip_filename = input(termcolor.colored("Enter Path Of Your zip file:- ", 'cyan'))
+    if not os.path.exists(zip_filename):
+        print(termcolor.colored("\n[ X ] File " + zip_filename + " was not found, Provide Valid FileName And Path!",
+                                'red'))
+        exit()
+    print(termcolor.colored("\n[*] Analyzing Zip File:- ", 'blue'), zip_filename)
+    time.sleep(2)
+    if zip_filename[-3:] == "zip":
+        print(termcolor.colored("\n[ ✔ ] Valid ZIP File Found...", 'green'))
+    else:
+        print(termcolor.colored("\n[ X ] This is not a valid .zip file...\n", 'red'))
+        exit()
+    pwd_filename = input(termcolor.colored("\nEnter Path Of Your Wordlist:- ", 'yellow'))
+
+    if not os.path.exists(pwd_filename):
+        print(termcolor.colored("\n[ X ] File " + pwd_filename + " was not found, Provide Valid FileName And Path!",
+                                'red'))
+        exit()
+    with open(pwd_filename, "rb") as passwords:
+        passwords_list = passwords.readlines()
+        total_passwords = len(passwords_list)
+        my_zip_file = zipfile.ZipFile(zip_filename)
+        for index, password in enumerate(passwords_list):
+            try:
+                my_zip_file.extractall(path="Extracted Folder", pwd=password.strip())
+                print(colored("\n{***********************SUCCESS***********************}", 'green'))
+                print(colored("[ ✔ ] ZIP FILE Password Found:- ", 'cyan'), password.decode().strip())
+                break
+            except:
+                helo = round((index / total_passwords) * 100, 2)
+                if helo == '100%':
+                    print(colored("[ X ] ALL ATTEMPTS FAILED", 'red'))
+                else:
+                    print(colored(f"[*] Trying password:- {password.decode().strip()} ", 'green'))
+                continue
+def catc():
+    try:
+        if platform.system().startswith("Linux"):
+            linuxpdf()
+        elif platform.system().startswith("Windows"):
+            winpdf()
+    except KeyboardInterrupt:
+        print(termcolor.colored("\nYou Pressed The Exit Button!", 'red'))
+        quit()
+
+
+catc()
